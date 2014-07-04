9372ceac72b1edad972fec3a5d2ee72373be416d
LI Yu <liyu@clustertech.com>
2014-07-04 15:16:48 +0800

    pdf generation scripts for getting_started, mix_otp and meta.
    As I found that the files in
    https://groups.google.com/forum/#!topic/elixir-lang-core/aRzIbxswsLQ
    are gone. I come up with my own scripts to generate nice pdf version
    for elixir getting_started documents.

---
+++ _pdf/preprocess.js
@@ -0,0 +1,70 @@
+//Simple js code to preprocess the combined markdown file. Things could break
+//when there is sth not considered.
+//
+//author: Li, Yu (liyu1981@gmail.com)
+
+var lines = require('fs').readFileSync('all.markdown').toString().split('\n');
+var reHeader = /^---$/;
+var reCodeStart = /^```(.*)/;
+var reCodeEnd = /^```$/;
+var reSection = /^(#+)\s(\d+\.\d+)/;
+var inHeaderMode = false;
+var inCodeMode = false;
+var curPageTitle = null;
+lines.forEach(function(line) {
+  //console.log('** will do:', line);
+  if (line.indexOf('<div class="toc"></div>') >= 0) {
+    // skip
+    return;
+  }
+
+  if (line.indexOf('{{ page.title }}') >= 0) {
+    var l = line.replace('{{ page.title }}', curPageTitle.toString());
+    console.log(l);
+    return;
+  }
+
+  var l = line.trim();
+
+  if (inHeaderMode === true) {
+    if (reHeader.test(l)) {
+      inHeaderMode = false;
+    }
+    var kv = l.split(':');
+    if (kv[0] === 'title') {
+      curPageTitle = kv[1].trim();
+      // remove the leading number
+      var r = /^\d+\s/.exec(curPageTitle);
+      if (r) {
+        curPageTitle = curPageTitle.substring(r.index + r[0].length);
+      }
+      //console.log('change title:', curPageTitle);
+    }
+    return;
+  } else {
+    if (reHeader.test(l)) {
+      inHeaderMode = true;
+      return;
+    }
+    var r = reCodeStart.exec(l);
+    if (r && inCodeMode === false) {
+      console.log('\\begin{minted}[mathescape, linenos, numbersep=5pt, gobble=2, frame=lines, framesep=2mm]{' + (r[1] ? r[1] : 'ex' ) + '}');
+      inCodeMode = true;
+      return;
+    }
+    if (reCodeEnd.test(l)) {
+      console.log('\\end{minted}');
+      inCodeMode = false;
+      return;
+    }
+
+    // remove the numbers in title, such as ## 1.1 Installing Erlang => ## Installing Erlang
+    r = reSection.exec(l);
+    if (r) {
+      console.log(l.replace(r[2], ''));
+      return;
+    }
+
+    console.log(line);
+  }
+});
