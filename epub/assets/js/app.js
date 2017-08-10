e38882733c9348dddd58893bb366126fad869c40
Milton Mazzarri <milmazz@gmail.com>
2017-08-09 23:56:59 -0500

    Update highlightjs configuration

--- epub/assets/js/app.js
+++ epub/assets/js/app.js
@@ -1,17 +1,11 @@
 // Dependencies
 // ------------
 
-import $ from 'jquery'
 import hljs from 'highlight.js/build/highlight.pack'
 
-window.$ = $
-
-$(() => {
-  // Setup Highlight.js
-  hljs.configure({
-    tabReplace: '    ', // 4 spaces
-    languages: []       // disable auto-detect
-  })
-
-  hljs.initHighlighting()
+// Setup Highlight.js
+hljs.configure({
+  tabReplace: '    ' // 4 spaces
 })
+
+hljs.initHighlightingOnLoad()
